1684ff49165ff2f1246731e9e64549be1129d25a
ferrapa1 <ferrapa1@students.zhaw.ch>
2026-05-30 07:43:09 +0200

    simplify user persistence adapter

--- backend/src/main/java/ch/zhaw/ssdd/pas/adapters/outbound/jpa/user/adapter/UserPersistenceAdapter.java
+++ backend/src/main/java/ch/zhaw/ssdd/pas/adapters/outbound/jpa/user/adapter/UserPersistenceAdapter.java
@@ -1,22 +1,12 @@
 package ch.zhaw.ssdd.pas.adapters.outbound.jpa.user.adapter;
 
 import ch.zhaw.ssdd.pas.adapters.outbound.jpa.AddressEntity;
-import ch.zhaw.ssdd.pas.adapters.outbound.jpa.user.repository.UserEntityRepository;
 import ch.zhaw.ssdd.pas.domain.user.model.Address;
 import ch.zhaw.ssdd.pas.ports.outbound.UserPersistence;
-import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-//TODO
-@Service
 public class UserPersistenceAdapter implements UserPersistence {
-
-    private final UserEntityRepository userEntityRepository;
-
-    public UserPersistenceAdapter(UserEntityRepository userEntityRepository) {
-        this.userEntityRepository = userEntityRepository;
-    }
 
     protected static AddressEntity mapAddressFromDomain(Address address) {
         AddressEntity addressEntity = new AddressEntity();
